b4dd4e99215de37051e7eb9f029d8b64e6ee6e1c
onlysad-boy <padamshree98@gmail.com>
2021-02-26 00:09:15 +0530

    Working Substitution Model(Random)

--- backend/utils/substitutions.js
+++ backend/utils/substitutions.js
@@ -1,48 +1,88 @@
 function generateSub(date, timetable, absentList, teachersList) {
+  let substitutions = [];
   let subTable = [];
-  let subReplacements = [];
   let absentLookup = {};
   let presentLookup = {};
   let subDate = new Date(date);
   let day = subDate.getDay();
   let numPeriods = timetable.length;
   const [dayStart, dayEnd] = getSubDay(day, numPeriods);
+  // Extracting Specific Day's Timetable
   if (dayStart >= 0 && dayEnd > 0) {
     for (i = dayStart; i < dayEnd; i++) {
       subTable.push(timetable[i]);
     }
   }
-
+  // Creating Lookup list for Absent Teachers, Not Necessary, Better Optimize
   absentList.forEach((teacher) => {
     absentLookup[teacher._id] = teacher;
   })
-
+  // Creating Lookup list for Present Teachers
   teachersList.forEach((teacher) => {
     if (absentLookup[teacher._id]) {
       console.log(`${teacher.teacherName} is Absent`);
     } else {
-      presentLookup[teacher._id] = teacher;
+      presentLookup[teacher._id] = { teacherId: teacher._id, teacher: teacher.teacherName };
     }
   })
-
+  console.log('');
+  // Looping Over Day's Timetable, Finding Absent Teachers
+  // And Removing Teacher From Present List if they already have a class
   subTable.forEach((period, index) => {
-    let replacements = {};
+    let totalPeriodsToReplace = 0;
     let checkReplacements = {...presentLookup};
+    let periodAbsentees = {};
+    let subAssignments = [];
     period.forEach((tuple) => {
+      const { className, classId } = tuple;
       if (absentLookup[tuple.teacherId]) {
-        console.log(tuple.teacherName, tuple.className, 'Period No. ', index + 1);
+        totalPeriodsToReplace++;
+        periodAbsentees[tuple.teacherId] = { ...absentLookup[tuple.teacherId], className: className, classID: classId };
       }
       if (checkReplacements[tuple.teacherId]) {
         delete checkReplacements[tuple.teacherId];
       }
     })
-    replacements = {...checkReplacements};
-    subReplacements.push(replacements);
+    // Randomly Assigning An Absent Teachers's Class
+    // A teacher from Free Teachers
+    while (periodAbsentees && Object.keys(periodAbsentees).length !== 0
+    && periodAbsentees.constructor === Object) {
+      let absent = randomKey(periodAbsentees);
+      let replacement = randomKey(checkReplacements);
+      const { className, label, _id, classID } = periodAbsentees[absent];
+      checkReplacements[replacement] = { ...checkReplacements[replacement],
+        periodNo: (index + 1),
+        substitutionClass: className,
+        substitutionClassId: classID,
+        absentTeacher: label,
+        absentTeacherId: _id,
+       };
+      subAssignments.push(checkReplacements[replacement]);
+      delete checkReplacements[replacement];
+      delete periodAbsentees[absent];
+    }
+    substitutions.push(subAssignments);
+    if (checkReplacements && Object.keys(checkReplacements).length!==0) {
+      Object.keys(checkReplacements).map((key) => {
+        console.log(`${checkReplacements[key].teacher} still free for period no ${index+1}`);
+        return checkReplacements[key];
+      });
+    } else {
+      console.log(`No Free Teacher for period ${index + 1}`);
+    }
+    // if (index === 0) {
+    //   console.log(substitutions);
+    // }
+    // let repCount = Object.keys(checkReplacements).map(key => {
+    //   return checkReplacements[key];
+    // })
+    // subReplacements.push(repCount);
+    // console.log(`Total periods to replace in period ${index + 1} are ${totalPeriodsToReplace}`);
+    // console.log(`Available teachers for replacements are ${repCount.length}`);
+    console.log(subAssignments);
+    console.log('  ')
   })
-  // subTable.forEach((period, index))
-  // return subTable;
-  console.log(subReplacements);
-  // console.log(presentTeachers);
+  // console.log(substitutions);
   return 0;
 }
 
@@ -61,7 +101,11 @@ function getSubDay(day, numPeriods) {
   return [dayStart, dayEnd];
 }
 
+function randomKey(obj) {
+let keys = Object.keys(obj);
+return keys[keys.length * Math.random() << 0];
+};
+
 module.exports = {
   generateSub,
-  getSubDay,
 };
